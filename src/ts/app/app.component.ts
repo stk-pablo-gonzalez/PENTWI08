b1ad1fec391d28331130e0055b46119e314fc7a2
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2018-04-03 17:06:29 -0500

    Working with FormControl

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from "@angular/core";
+import { FormControl } from "@angular/forms";
 
 @Component({
     selector: "main",
-    template: "<span>{{message}}</span>",
+    template: `
+        <div>
+            <label for="first-name-input">First Name:</label>
+            <input type="text" id="first-name-input" [formControl]="firstNameInput">
+            <br>Value: {{firstNameInput.value}}
+        </div>
+        <div>
+            <label for="last-name-input">Last Name:</label>
+            <input type="text" id="last-name-input" [formControl]="lastNameInput">
+            <br>Value: {{lastNameInput.value}}
+        </div>
+    `,
 })
 export class AppComponent {
+    public firstNameInput: FormControl = new FormControl("");
+    public lastNameInput: FormControl = new FormControl("");
 
     public message: string = "Hello World!";
 
