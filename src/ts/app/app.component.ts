073c85a5e49ef02622eed829a08254ebdf80a796
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2018-04-04 00:24:31 -0500

    Setting address groups

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,25 +1,71 @@
 import { Component } from "@angular/core";
-import { FormControl } from "@angular/forms";
+import { FormControl, FormGroup, Validators, FormArray } from "@angular/forms";
 
 @Component({
     selector: "main",
     template: `
+    <form [formGroup]="profileForm">
+    <h1>Profile</h1>
         <div>
             <label for="first-name-input">First Name:</label>
-            <input type="text" id="first-name-input" [formControl]="firstNameInput">
-            <br>Value: {{firstNameInput.value}}
+            <input type="text" id="first-name-input" formControlName="firstNameInput">
+            <br>Value: {{profileForm.controls['firstNameInput'].value}}
         </div>
         <div>
             <label for="last-name-input">Last Name:</label>
-            <input type="text" id="last-name-input" [formControl]="lastNameInput">
-            <br>Value: {{lastNameInput.value}}
+            <input type="text" id="last-name-input" formControlName="lastNameInput">
+            <br>Value: {{profileForm.controls['lastNameInput'].value}}
         </div>
+        <div formArrayName="addressGroups">
+            <fieldset *ngFor="let addressGroup of profileForm.controls['addressGroups'].controls" [formGroup]="addressGroup">
+                <h3>Address</h3>
+                <div>
+                    <label for="street-input">Street:</label>
+                    <input type="text" id="street-input" formControlName="streetInput">
+                    <br>Value: {{addressGroup.controls['streetInput'].value}}
+                </div>
+                <div>
+                    <label for="city-input">city:</label>
+                    <input type="text" id="city-input" formControlName="cityInput">
+                    <br>Value: {{addressGroup.controls['cityInput'].value}}
+                </div>
+                <div>
+                    <label for="state-input">state:</label>
+                    <input type="text" id="state-input" formControlName="stateInput">
+                    <br>Value: {{addressGroup.controls['stateInput'].value}}
+                </div>
+                <div>
+                    <label for="zip-code-input">zipCode:</label>
+                    <input type="text" id="zip-code-input" formControlName="zipCodeInput">
+                    <br>Value: {{addressGroup.controls['zipCodeInput'].value}}
+                </div>
+            </fieldset>
+        </div>
+        <button type="button" (click)="addAddressGroup()">Add Address</button>
+    </form>
     `,
 })
 export class AppComponent {
-    public firstNameInput: FormControl = new FormControl("");
-    public lastNameInput: FormControl = new FormControl("");
+    public profileForm: FormGroup = new FormGroup({
+        firstNameInput: new FormControl("", Validators.required),
+        lastNameInput: new FormControl(""),
+        addressGroups: new FormArray([
+            this.newAddressGroup()
+        ])
+    });
 
-    public message: string = "Hello World!";
+    public addAddressGroup() {
+        const fa = this.profileForm.controls["addressGroups"] as FormArray;
 
+        fa.push(this.newAddressGroup());
+    }
+
+    private newAddressGroup() {
+        return new FormGroup({
+            streetInput: new FormControl(""),
+            cityInput: new FormControl(""),
+            stateInput: new FormControl(""),
+            zipCodeInput: new FormControl(""),
+        });
+    }
 }
