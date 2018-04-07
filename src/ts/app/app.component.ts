ed6d4e1e6daf0615b43aa7d391366777455b5419
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2018-04-07 14:14:23 -0500

    one-way data binding

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -3,10 +3,14 @@ import { Component } from "@angular/core";
 @Component({
     selector: "main",
     template: `
-        <div>{{message}}</div>
+        <div>Old Message: {{message}}</div>
+        <div>New Message: {{newMessage}}</div>
         <div>
             <label for="message-input">Message:</label>
             <input type="text" id="message-input" name="messageInput" [(ngModel)]="message">
+            <br/>
+            <label for="message2-input">Message:</label>
+            <input type="text" id="message2-input" name="message2Input" [ngModel]="message" (ngModelChange)="newMessage = $event">
         </div>
     `,
 })
