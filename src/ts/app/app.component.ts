1a89d4e1a300b4cf3fb5ec1b4fe801b37e93a640
Pablo Gonzalez <pablo@Pablos-Mac-mini.local>
2018-04-14 14:07:50 -0500

    Validation with reactive forms

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,26 +1,21 @@
 import { Component } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: "main",
-    styles: [
-        "input.ng-touched.ng-invalid { border: 2px solid red; margin: 5px; }",
-        "input + span { display: none; }",
-        "input.ng-invalid.ng-touched + span { display: inline; }",
-    ],
     template: `
-        <form #f="ngForm">
-            <span *ngIf="f.valid">The Form is valid</span>
-            <fieldset #g="ngModelGroup" ngModelGroup="someGroup">
-                <span *ngIf="g.valid">The model group is valid</span>
-                <input #c="ngModel" type="text" id="message-input" name="messageInput" [(ngModel)]="message" required>
-                <span>Message is required.</span>
-                <span *ngIf="c.valid">Message is valid.</span>
-            </fieldset>
-        </form>
+        <div>
+            <label for="message-input">Message:</label>
+            <input type="text" id="message-input" [formControl]="messageControl">
+            <span *ngIf="messageControl.invalid && messageControl.touched">
+                Please enter a message.
+            </span>
+        </div>
     `,
 })
 export class AppComponent {
 
     public message: string = "";
+    public messageControl: FormControl = new FormControl("", Validators.required);
 
 }
