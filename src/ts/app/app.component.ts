75a97809fe7939c4fed53fb9849ae2169c506502
Pablo Gonzalez <pablo@Pablos-Mac-mini.local>
2018-04-15 23:28:25 -0500

    Conditional required validation

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -5,16 +5,21 @@ import { Component } from "@angular/core";
     styles: [
         "input.ng-touched.ng-invalid { border: 2px solid red; margin: 5px; }",
         "input + span { display: none; }",
-        "input.ng-invalid.ng-touched + span { display: inline; }",
+        "input.ng-invalid + span { display: inline; }",
     ],
     template: `
-        <form #f="ngForm">
-            <span *ngIf="f.valid">The Form is valid</span>
-            <fieldset #g="ngModelGroup" ngModelGroup="someGroup">
-                <span *ngIf="g.valid">The model group is valid</span>
-                <input #c="ngModel" type="text" id="message-input" name="messageInput" [(ngModel)]="message" required>
-                <span>Message is required.</span>
-                <span *ngIf="c.valid">Message is valid.</span>
+        <form>
+            <fieldset ngModelGroup="someGroup">
+                <div>
+                    <label for="make-required">Make Required:</label>
+                    <input type="checkbox" id="make-required" [(ngModel)]="makeRequired" name="makeRequiredInput">
+                </div>
+                <div>
+                    <label for="message-input">Message:</label>
+                    <input type="text" id="message-input" name="messageInput" [(ngModel)]="message"
+                        [required]="makeRequired">
+                    <span>Message is required.</span>
+                </div>
             </fieldset>
         </form>
     `,
