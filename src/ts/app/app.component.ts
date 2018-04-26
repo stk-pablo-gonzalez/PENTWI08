29741604eb61650d3ac737b2941d26af451758e1
Pablo Gonzalez <pablogr.gonzalez@gmail.com>
2018-04-26 13:01:24 -0500

    Using template references

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,10 +2,23 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    template: "<span>{{message}}</span>",
+    template: `
+        <form #contactForm="ngForm" id="contact-form" novalidate>
+            <div>Submitted: {{contactForm.submitted}}</div>
+            <fieldset #addressGroup="ngModelGroup" ngModelGroup="address">
+                <div>Address Group valid: {{addressGroup.valid}}</div>
+                <div>
+                    <input type="text" #streetInput="ngModel" [(ngModel)]="street" name="streetInput" required>
+                    <span *ngIf="streetInput.invalid && streetInput.touched">
+                        Street is required.
+                    </span>
+                    {{street}}
+                </div>
+            </fieldset>
+            <button type="submit">Submit</button>
+        </form>
+    `,
 })
 export class AppComponent {
-
-    public message: string = "Hello World!";
-
+    public street: string = "";
 }
